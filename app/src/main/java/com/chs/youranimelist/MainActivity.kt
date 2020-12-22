63bc0867cb723c4683fb9b984540f4ecb6619288
HeonSeokCha <hs960829@gmail.com>
2020-12-22 23:12:12 +0900

    first commit. (#2)

--- app/src/main/java/com/chs/youranimelist/MainActivity.kt
+++ app/src/main/java/com/chs/youranimelist/MainActivity.kt
@@ -13,16 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
