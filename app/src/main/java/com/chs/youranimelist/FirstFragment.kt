63dc7395e4b98d6379f8b5965da1601afe3014cc
HeonSeokCha <hs960829@gmail.com>
2020-12-28 21:39:37 +0900

    working with UI.

--- app/src/main/java/com/chs/youranimelist/FirstFragment.kt
+++ app/src/main/java/com/chs/youranimelist/FirstFragment.kt
@@ -17,15 +17,11 @@ class FirstFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 }
