8ccc8f6fdc592848a9b05e5454762b43fc5bcbf1
HyeonSeokCha <hs960829@gmail.com>
2022-06-11 18:00:31 +0900

    initial Commit remove legacy xml dir. add Compose.

--- app/src/main/java/com/chs/youranimelist/data/datasource/JikanService.kt
+++
@@ -1,7 +0,0 @@
-package com.chs.youranimelist.data.datasource
-
-import com.chs.youranimelist.data.model.AnimeDetails
-
-interface JikanService {
-    suspend fun getAnimeTheme(malId: Int): AnimeDetails?
-}
