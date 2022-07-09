1aee97b92679276031b1faeae3f795455bacdd09
hyeonseok <hs960829@gmail.com>
2022-07-09 23:28:01 +0900

    Modify AnimeDetailScreen adding TabLayout.

---
+++ app/src/main/java/com/chs/youranimelist/presentation/browse/anime/AnimeCharaScreen.kt
@@ -0,0 +1,17 @@
+package com.chs.youranimelist.presentation.browse.anime
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AnimeCharaScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(text = "AnimeCharaScreen")
+    }
+}
--- app/src/main/java/com/chs/youranimelist/presentation/browse/anime/AnimeDetailScreen.kt
+++ app/src/main/java/com/chs/youranimelist/presentation/browse/anime/AnimeDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.chs.youranimelist.presentation.browse.anime
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -8,8 +9,10 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -21,7 +24,13 @@ import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.chs.youranimelist.AnimeDetailQuery
 import com.chs.youranimelist.presentation.browse.BrowseScreen
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun AnimeDetailScreen(
     id: Int,
@@ -32,6 +41,8 @@ fun AnimeDetailScreen(
 
     val state = viewModel.state
     val context = LocalContext.current
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(viewModel, context) {
         viewModel.getAnimeDetailInfo(id)
@@ -43,34 +54,58 @@ fun AnimeDetailScreen(
     ) {
         DetailBanner(state.animeDetailInfo)
 
-        Button(
-            onClick = {
-                navController.navigate(
-                    "${BrowseScreen.AnimeDetailScreen.route}/$id/$idMal"
-                )
-            }
-        ) {
-            Text("AnimeDetail")
-        }
+        val tabList = listOf(
+            "OVERVIEW",
+            "CHARACTER",
+            "RECOMMEND"
+        )
 
-        Button(
-            onClick = {
-                navController.navigate(
-                    "${BrowseScreen.CharacterDetailScreen.route}/$id"
+        Spacer(
+            modifier = Modifier
+                .padding(top = 16.dp)
+        )
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            backgroundColor = Color.White,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    modifier = Modifier
+                        .pagerTabIndicatorOffset(pagerState, tabPositions),
+                    color = Color.Blue
                 )
             }
         ) {
-            Text("Character")
+            tabList.forEachIndexed { index, s ->
+                Tab(
+                    text = {
+                        Text(
+                            text = tabList[index]
+                        )
+                    },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                )
+            }
         }
-
-        Button(
-            onClick = {
-                navController.navigate(
-                    "${BrowseScreen.StudioDetailScreen.route}/$id"
-                )
-            }
+        HorizontalPager(
+            count = tabList.size,
+            state = pagerState
         ) {
-            Text("Studio")
+            when (this.currentPage) {
+                0 -> {
+                    AnimeOverViewScreen()
+                }
+                1 -> {
+                    AnimeCharaScreen()
+                }
+                2 -> {
+                    AnimeRecScreen()
+                }
+            }
         }
     }
 
@@ -100,22 +135,25 @@ fun DetailBanner(animeInfo: AnimeDetailQuery.Data?) {
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
-        FloatingActionButton(
-            modifier = Modifier
-                .align(Alignment.Center),
-            onClick = { /*TODO*/ }
-        ) {
-            Icon(Icons.Filled.PlayArrow, null)
+        if (animeInfo?.media?.trailer != null) {
+            FloatingActionButton(
+                modifier = Modifier
+                    .align(Alignment.Center),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Filled.PlayArrow, null)
+            }
         }
+
         AsyncImage(
             modifier = Modifier
                 .width(130.dp)
                 .height(180.dp)
                 .padding(
                     start = 8.dp,
-
                 )
-                .align(Alignment.BottomStart),
+                .align(Alignment.BottomStart)
+                .clip(RoundedCornerShape(5.dp)),
             model = animeInfo?.media?.coverImage?.extraLarge,
             contentDescription = null,
             contentScale = ContentScale.Crop
@@ -124,7 +162,10 @@ fun DetailBanner(animeInfo: AnimeDetailQuery.Data?) {
         Column(
             modifier = Modifier
                 .align(Alignment.BottomStart)
-                .padding(start = 154.dp)
+                .padding(
+                    start = 154.dp,
+                    bottom = 8.dp
+                )
         ) {
             Text(
                 text = animeInfo?.media?.title?.english
