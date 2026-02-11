724244bc3f5f2a4495948bc0c7ce203219cb1243
HeonSeokCha <hs960829@gmail.com>
2026-02-11 21:41:08 +0900

    refactor SearchBar.

--- shared/src/androidMain/kotlin/com/chs/youranimelist/MainActivity.kt
+++ shared/src/androidMain/kotlin/com/chs/youranimelist/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.ui.platform.LocalContext
 import com.chs.youranimelist.presentation.UiConst
 import com.chs.youranimelist.presentation.main.MainApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             val context = LocalContext.current
--- shared/src/commonMain/kotlin/com/chs/youranimelist/presentation/main/AppBar.kt
+++ shared/src/commonMain/kotlin/com/chs/youranimelist/presentation/main/AppBar.kt
@@ -1,13 +1,17 @@
 package com.chs.youranimelist.presentation.main
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.input.clearText
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
@@ -19,10 +23,12 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.unit.dp
 import com.chs.youranimelist.presentation.ui.theme.Red500
 import com.chs.youranimelist.res.Res
 import com.chs.youranimelist.res.app_name
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -90,140 +96,6 @@ fun AppBar(
     }
 }
 
-//
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun SearchAppBar(
-//    modifier: Modifier,
-//    onSearch: (String) -> Unit,
-//    onBack: () -> Unit,
-//    searchHistoryList: List<String>,
-//    onDeleteSearchHistory: (String) -> Unit
-//) {
-//    var isShowDialog by remember { mutableStateOf(false) }
-//    var searchBarState by remember { mutableStateOf(false) }
-//    var textState by remember { mutableStateOf("") }
-//    val scope = rememberCoroutineScope()
-//    val textFieldState = rememberTextFieldState()
-//    val inputField =
-//        @Composable {
-//            SearchBarDefaults.InputField(
-//                query = textState,
-//                expanded = searchBarState,
-//                onExpandedChange = { searchBarState = it },
-//                onQueryChange = { textState = it },
-//                onSearch = {
-//                    scope.launch {
-//                        searchBarState = false
-//                    }
-//                    onSearch(it)
-//                },
-//                placeholder = { Text("Search here...") },
-//                leadingIcon = {
-//                    IconButton(
-//                        onClick = {
-//                            if (searchBarState) {
-//                                scope.launch { searchBarState = false }
-//                            } else onBack()
-//                        }
-//                    ) {
-//                        Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = "Back")
-//                    }
-//                },
-//                trailingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
-//            )
-//        }
-//
-//    SearchBar(
-//        modifier = modifier
-//            .fillMaxWidth()
-//            .height(TopAppBarDefaults.MediumAppBarCollapsedHeight),
-//        inputField = inputField,
-//        expanded = searchBarState,
-//        shape = RectangleShape,
-//        onExpandedChange = { it -> searchBarState = it }
-//    ) {
-//        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-//            items(searchHistoryList) { title ->
-//                Row(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .padding(all = 14.dp)
-//                        .combinedClickable(
-//                            onClick = {
-//                                textFieldState.edit {
-//                                    delete(0, length)
-//                                    append(title)
-//                                }
-//                                searchBarState = false
-//                                onSearch(title)
-//                            },
-//                            onLongClick = {
-//                                textFieldState.edit {
-//                                    delete(0, length)
-//                                    append(title)
-//                                }
-//                                isShowDialog = true
-//                            }
-//                        )
-//                ) {
-//                    Icon(
-//                        modifier = Modifier.padding(end = 10.dp),
-//                        imageVector = Icons.Default.History,
-//                        contentDescription = null
-//                    )
-//                    Text(text = title)
-//                }
-//            }
-//        }
-//
-//    }
-//
-//    SearchBar(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .height(TopAppBarDefaults.MediumAppBarCollapsedHeight),
-//        shape = RectangleShape,
-//        state = searchBarState,
-//        inputField = inputField
-//    )
-//
-//    ExpandedFullScreenSearchBar(
-//        state = searchBarState,
-//        inputField = inputField,
-//        colors = SearchBarDefaults.colors(
-//            containerColor = Red200,
-//            inputFieldColors = TextFieldDefaults.colors()
-//        )
-//    ) {
-//
-//    }
-//
-//    if (isShowDialog) {
-//        AlertDialog(
-//            onDismissRequest = { isShowDialog = false },
-//            title = { Text(text = textFieldState.text.toString()) },
-//            text = { Text(text = "Are You Sure Delete Search History?") },
-//            confirmButton = {
-//                Button(
-//                    onClick = {
-//                        isShowDialog = false
-//                        onDeleteSearchHistory(
-//                            textFieldState.text.toString()
-//                        )
-//                    }) {
-//                    Text("Delete")
-//                }
-//            },
-//            dismissButton = {
-//                Button(onClick = { isShowDialog = false }) {
-//                    Text("Cancel")
-//                }
-//            }
-//        )
-//    }
-//}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchAppBar(
@@ -233,36 +105,35 @@ fun SearchAppBar(
 ) {
     var isShowDialog by remember { mutableStateOf(false) }
     var text by remember { mutableStateOf("") }
-    var isSearchActive by remember { mutableStateOf(false) }
+    val searchBarState = rememberSearchBarState()
+    val textFieldState = rememberTextFieldState()
+    val scope = rememberCoroutineScope()
+    val inputField =
+        @Composable {
+            SearchBarDefaults.InputField(
+                searchBarState = searchBarState,
+                textFieldState = textFieldState,
+                onSearch = {
+                    val query = textFieldState.text.toString()
+                    scope.launch { searchBarState.animateToCollapsed() }
+                    onSearch(query)
+                },
+                placeholder = {
+                    Text(modifier = Modifier.clearAndSetSemantics {}, text = "Search...")
+                },
+                trailingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+            )
+        }
 
     SearchBar(
         modifier = Modifier
-            .fillMaxWidth(),
-        query = text,
-        onQueryChange = { text = it },
-        onSearch = {
-            isSearchActive = false
-            onSearch(it)
-        },
-        active = isSearchActive,
-        onActiveChange = { isSearchActive = it },
-        placeholder = { Text("Search here...") },
-        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
-        trailingIcon = {
-            IconButton(onClick = {
-                if (text.isNotEmpty()) {
-                    text = ""
-                } else {
-                    isSearchActive = false
-                }
-            }) {
-                Icon(
-                    Icons.Default.Close,
-                    contentDescription = null
-                )
-            }
-        }
-    ) {
+            .padding(horizontal = 16.dp)
+            .statusBarsPadding(),
+        inputField = inputField,
+        state = searchBarState,
+    )
+
+    ExpandedFullScreenSearchBar(state = searchBarState, inputField = inputField) {
         LazyColumn(
             contentPadding = PaddingValues(
                 start = 16.dp,
@@ -276,11 +147,19 @@ fun SearchAppBar(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(all = 14.dp)
-                        .clickable {
-                            text = title
-                            isSearchActive = false
-                            onSearch(title)
-                        }
+                        .combinedClickable(
+                            enabled = true,
+                            onClick = {
+                                textFieldState.clearText()
+                                textFieldState.edit { append(title) }
+                                scope.launch { searchBarState.animateToCollapsed() }
+                                onSearch(title)
+                            },
+                            onLongClick = {
+                                text = title
+                                isShowDialog = true
+                            }
+                        )
                 ) {
                     Icon(
                         modifier = Modifier.padding(end = 10.dp),
